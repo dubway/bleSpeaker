73572627b6f68f056d4a635b8b1723a86ceb1cee
Andy Sigler <andrewsigler1@gmail.com>
2014-04-06 11:17:22 -0400

    made some classes

--- app.js
+++ app.js
@@ -25,23 +25,7 @@ bleno.on('stateChange', function(state) {
 ////////////////////////////////////
 ////////////////////////////////////
 
-var max = -40;
-var min = -70;
-var diff = max-min;
-
-function mapRSSI(rssi){
-  console.log(rssi);
-  rssi = Number(rssi);
-  rssi -= min;
-  rssi /= diff;
-  rssi = Math.floor(rssi*255);
-  if(rssi<0) rssi=0;
-  else if(rssi>255) rssi = 255;
-  sendBrightness(rssi);
-}
-
 function sendBrightness(val){
-  //console.log(' ------ >>>>>  sending brightness: '+val);
   client.send('/rssi', val);
 }
 
@@ -50,29 +34,40 @@ function sendBrightness(val){
 ////////////////////////////////////
 
 var delay = 100;
-var andy_timeStamp;
 var blue_timeStamp;
 
 function start(){
-  andy_timeStamp = new Date().getTime();
   blue_timeStamp = new Date().getTime();
-  setInterval(startInterval,delay);
+  setInterval(update,delay);
   noble.startScanning([], true); // start scanning
 }
 
-noble.on('discover', function(peripheral){
-  if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('Andy')>=0){
-    var now = new Date().getTime();
-    var delay = now-andy_timeStamp;
-    //console.log('discovered --> '+peripheral.advertisement.localName + ' @ '+peripheral.rssi+'_rssi & '+delay+'_delay');
-    andy_timeStamp = now;
+function update(){
+
+  if(phone){
+    phone.update();
+    console.log(phone.distance);
+    sendBrightness(Math.floor(phone.distance)); // send to Processing over OSC
   }
-  else if(peripheral.advertisement.localName && peripheral.advertisement.localName.indexOf('Light')>=0){
-    var now = new Date().getTime();
-    var delay = now-blue_timeStamp;
-    //console.log('discovered --> '+peripheral.advertisement.localName + ' @ '+peripheral.rssi+'_rssi & '+delay+'_delay');
-    blue_timeStamp = now;
-    mapRSSI(peripheral.rssi);
+  for(var n in speakers){
+    speakers[n].update();
+  }
+
+  var tempRSSI = phone ? phone.rssi : 'nada';
+  bleno.startAdvertising('s_'+tempRSSI);
+}
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+
+noble.on('discover', function(peripheral){
+  var tag = peripheral.advertisement.localName;
+  if(tag && tag.indexOf('s_')===0){
+    handleSpeaker(peripheral,tag);
+  }
+  else if(tag && tag.indexOf('Light')>=0){
+    handPhone(peripheral);
   }
 });
 
@@ -80,9 +75,58 @@ noble.on('discover', function(peripheral){
 ////////////////////////////////////
 ////////////////////////////////////
 
-function startInterval(){
-  myName++;
-  bleno.startAdvertising('Andy_'+myName);
+var phone;
+var speakers = {};
+
+function handleSpeaker(p,m){
+  if(!speakers[p.uuid]){
+    speakers[p.uuid] = new Device(p.uuid);
+    console.log('found Speaker with UUID --> '+p.uuid);
+  }
+  speakers[p.uuid].receiveRSSI( p.rssi , m.split('_')[1] );
+}
+
+function handPhone(p){
+  if(!phone){
+    phone = new Device(p.uuid);
+    console.log('found a Phone with UUID --> '+p.uuid);
+  }
+  phone.receiveRSSI(p.rssi);
+}
+
+////////////////////////////////////
+////////////////////////////////////
+////////////////////////////////////
+
+function Device(_uuid){
+  this.uuid = _uuid;
+  this.rssi;
+  this.distance = 0;
+  this.timeStamp;
+  this.phoneRssi;
+}
+
+Device.prototype.update = function(){
+  // global = global + ( ( new - global ) / slide );
+  var slide = 10;
+  this.distance = this.distance + ( (this.rssi - this.distance) / slide);
+  if(this.distance<0) this.distance = 0;
+  if(this.distance>255) this.distance = 255;
+}
+
+Device.prototype.receiveRSSI = function(_rssi, _phoneRssi){
+  this.mapRssi(_rssi);
+  if(_phoneRssi!='nada') this.phoneRssi = _phoneRssi;
+  this.timeStamp = new Date().getTime();
+}
+
+Device.prototype.mapRssi = function(_rssi){
+
+  var temp = Number(_rssi);
+
+  temp = Math.pow(10 , (temp/10)) * 1000000;
+
+  this.rssi = temp;
 }
 
 ////////////////////////////////////
