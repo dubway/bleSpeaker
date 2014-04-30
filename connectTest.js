26a2cb594f5eb451757f7181e6daac650f03bd8f
Andy Sigler <andrewsigler1@gmail.com>
2014-04-30 14:47:15 -0400

    thing

---
+++ connectTest.js
@@ -0,0 +1,44 @@
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+var util = require('util');
+
+var bleno = require('bleno');
+var noble = require('noble');
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+noble.on('stateChange', function(state) {
+  console.log('Noble -> stateChange: ' + state);
+
+  if(state==='poweredOn'){
+  	noble.startScanning([], true);
+  }
+});
+
+bleno.on('stateChange', function(state) {
+	if(state==='poweredOn'){
+  	bleno.startAdvertising('Speaker',['fffffffffffffffffffffffffffffff0'], function(error){
+  		console.log('error on startAdvertising');
+  	});
+  }
+});
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
+
+var counter = 0;
+
+noble.on('discover',function(periph){
+	if(periph.advertisement.localName==='LightBlue'){
+		console.log('LightBlue - '+(counter++));
+	}
+});
+
+////////////////////////////////
+////////////////////////////////
+////////////////////////////////
