0b7a3fb7e34c30c0be10b3741d5bfe0c9c74294c
Andy Sigler <andrewsigler1@gmail.com>
2014-05-01 03:19:00 -0400

    my b

--- pi_app.js
+++ pi_app.js
@@ -86,7 +86,7 @@ Phone.prototype.receiveRSSI = function(_rssi){
 ////////////////////////////////////
 ////////////////////////////////////
 
-var oscClient = new osc.Client('128.122.6.213', 8000);
+var oscClient = new osc.Client('128.122.6.213', 8001);
 var oscServer = new osc.Server(8001, '0.0.0.0');
 
 oscServer.on("message", function (msg, rinfo) {
